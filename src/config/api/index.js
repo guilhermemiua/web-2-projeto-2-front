f91334c54e089561154699a26aa08e4093158db9
guilhermemiua <guilhermemiua@gmail.com>
2021-05-16 19:19:20 -0300

    fix: fixed build

--- src/config/api/index.js
+++ src/config/api/index.js
@@ -1,6 +1,9 @@
 import axios from 'axios';
 
-const BASE_URL = 'http://localhost:3333';
+const BASE_URL =
+  process.env.NODE_ENV === 'production'
+    ? 'https://web2-back.herokuapp.com'
+    : 'http://localhost:3333';
 
 const api = axios.create({
   baseURL: BASE_URL,
